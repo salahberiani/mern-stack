c9ff110a4dac75a5e68aabad9fa5dc621cc8e807
salahberiani <salaheddinberiani@gmail.com>
2021-07-15 15:10:14 +0000

    server changes

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ hello: 'hello world' });
 });
+
+app.get('/api/test', (req, res) => {
+  res.json({ test: 'test world' });
+});
+
 
 app.listen(5000, () => {
   console.log('server listening on port 5000');
