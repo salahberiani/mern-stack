98ab502d6fd551c47409fe1d4921512f1fa23502
salahberiani <salaheddinberiani@gmail.com>
2021-07-15 16:23:41 +0100

    post deploy

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 app.use(express.json()); //Parse json bodies
 const app = express();
 
-<<<<<<< HEAD
 app.get('/api', (req, res) => {
   res.json({ hello: 'hello world' });
 });
@@ -15,46 +14,6 @@ app.get('/api/test', (req, res) => {
   res.json({ test: 'test world' });
 });
 
-
-=======
-app.get('/api/hi', (req, res) => {
-  res.json({ hello: 'hello world' });
-});
-
-app.post('/api/redeploy', validateSecret, (req, res) => {
-  exec('git pull && npm run post-deploy', (err, stdout, stderr) => {
-    if (err) {
-      //some err occurred
-      console.error(err);
-      res.status(403).send(err);
-    } else {
-      // the *entire* stdout and stderr
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-      res.status(200).send(`Auto deploy completed ${stdout} ${stderr}`);
-    }
-  });
-});
-
-function validateSecret(req, res, next) {
-  /**
-   * Passing an argument to next() in middleware
-   * throws an error to the error handler automatically
-   */
-
-  const payload = JSON.stringify(req.body);
-  if (!payload) {
-    return next('Request body empty');
-  }
-  let sig =
-    'sha1=' + crypto.createHmac('sha1', process.env.WEBHOOK_SECRET).update(payload).digest('hex');
-  if (req.headers['x-hub-signature'] == sig) {
-    return next();
-  } else {
-    return next('Signatures did not match');
-  }
-}
->>>>>>> 5109b03669749565eed89125ed48ad2972615511
 app.listen(5000, () => {
   console.log('server listening on port 5000');
 });
