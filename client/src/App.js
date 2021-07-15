24fe3479dd9d90c3565b66d6f4f6c38e31bd92ff
salahberiani <salaheddinberiani@gmail.com>
2021-07-15 17:17:57 +0100

    post deploy fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload with CI/CD.
         </p>
         <span>
           <span>Learn </span>
